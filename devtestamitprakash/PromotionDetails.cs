c49bea6f2673539b3c6637e5b8466f861cded51c
amitprakash <amitbcet2k15@outlook.com>
2020-07-12 00:07:00 +0530

    Code added for Engine  type A

--- devtestamitprakash/PromotionDetails.cs
+++ devtestamitprakash/PromotionDetails.cs
@@ -3,9 +3,24 @@ namespace devtestamitprakash
 {
     public class PromotionDetails
     {
-        public PromotionDetails()
-        {
+        
+            // Promo for type A
+            public int promoType1(int A)
+            {
 
-        }
+                if (A > 0 && A < 3)
+                {
+                    return A * 50;
+                }
+                else if (A >= 3)
+                {
+                    int org = (A % 3) * 50;
+                    int promoPrice = (A / 3) * 130;
+                    return org + promoPrice;
+                }
+                return 0;
+
+            }
+        
     }
 }
