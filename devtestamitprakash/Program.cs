eb163ba451572b56497dea940c4aa50f3d336f4d
iamitprakash <Amit9641#>
2020-07-11 23:50:38 +0530

    Initial check-in of module devtestamitprakash

---
+++ devtestamitprakash/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace devtestamitprakash
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
